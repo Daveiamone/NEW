c0680daeb3eec8a097c41f047e591c9092374cfc
Dave <davidvip3223@gmail.com>
2022-10-20 02:15:28 +0300

    random

--- ex003_Sum/Program.cs
+++ ex003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 4;
-int numberB = 6;
+﻿int numberA = new Random().Next(1, 10); // будет число от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberB + numberA;
 Console.WriteLine(result);
